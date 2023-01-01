20d28da82319bb4c493f3ad35708f8127182fd0f
shadik <shadikmahamudsohan@gmail.com>
2023-01-01 13:00:16 +0600

    home page pc vertion added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../CSS/Home.css';
 
 const Home = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content text-center">
-                <div className="max-w-md">
-                    <h1 className="text-5xl font-bold">Welcome to optimize recipes</h1>
-                    <p className="py-6">Find your favorite recipes!! </p>
-                    <button className="btn btn-primary"
-                        onClick={() => navigate("/about")}
-                    >About Us</button>
+        <div className="hero min-h-screen background justify-start" style={{ minHeight: "100vh" }}>
+            <div className='paper-container flex justify-center items-center'>
+                <div>
+                    <h1 className='paper-text'>Optimize Your <br /> Recipes</h1>
+                    <br />
+                    <button className='paper-btn hover-underline-animation mb-9'>Step 1: find your recipes</button><br />
+                    <button className='paper-btn hover-underline-animation mb-9'>Step 2: Create Your recipes</button><br />
+                    <button onClick={() => navigate("/about")} className='paper-btn hover-underline-animation mb-9'>Step 3: Know about us</button>
                 </div>
             </div>
+            <div className='text'></div>
         </div>
     );
 };
